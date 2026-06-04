dd5b33837653370f5ca9904922b80f859858dde6
unknown <maureecemarco@rocketmail.com>
2026-06-05 01:13:53 +0700

    Add validation authController email password and prisma

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,9 +7,12 @@ exports.register = async (req, res) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  if (email == "") return res.status(400).json({ message: "Email is required" });
+  if (!email) return res.status(400).json({ message: "Email is required" });
+  if (!email.match("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")) return res.status(400).json({ message: "Invalid format for Email" });
 
-  if (password == "") return res.status(400).json({ message: "Password is required" });
+  if (!password) return res.status(400).json({ message: "Password is required" });
+  if (password.length < 8) return res.status(400).json({ message: "Minimum 8 characters for Password" });
+
 
   const existingUser = await prisma.user.findUnique({
     where: { email }
@@ -19,12 +22,23 @@ exports.register = async (req, res) => {
     return res.status(400).json({ message: "Email already registered" })
   }
 
-  const user = await prisma.user.create({
-    data: { email, password: hashedPassword }
-  });
+  try {
+    const user = await prisma.user.create({
+      data: { email, password: hashedPassword }
+    });
 
-  const { password: _, ...safeUser } = user;
-  res.json(safeUser);
+    const { password: _, ...safeUser } = user;
+    res.json(safeUser);
+  } catch (error) {
+    return res.status(400).json({ message: "Failed to register, please try again later" })
+  }
+
+  // const user = await prisma.user.create({
+  //   data: { email, password: hashedPassword }
+  // });
+
+  // const { password: _, ...safeUser } = user;
+  // res.json(safeUser);
 };
 
 exports.login = async (req, res) => {
